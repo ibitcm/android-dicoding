1b33bdee61a72f3bab2a22c666fe9ea5d7d46b4e
irwan <ibitcm@gmail.com>
2019-04-04 11:24:41 +0800

    first commmit

---
+++ app/src/main/java/id/co/interbit/barvolume/MainActivity.java
@@ -0,0 +1,37 @@
+package id.co.interbit.barvolume;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText edtWidth, edtHeight, edtLength;
+    Button btnCalculate;
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLength = findViewById(R.id.edt_length);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.btn_calculate){
+
+
+        }
+    }
+}
